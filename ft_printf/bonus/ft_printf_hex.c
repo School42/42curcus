a169b415255e8881be571f7e3d4f446d3c3f85f7
Tauk Tauk Lin <talin@z2t7c3.42bangkok.com>
2024-10-04 09:27:04 +0700

    final

--- ft_printf/bonus/ft_printf_hex.c
+++ ft_printf/bonus/ft_printf_hex.c
@@ -6,7 +6,7 @@
 /*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 13:29:19 by talin             #+#    #+#             */
-/*   Updated: 2024/10/03 13:09:30 by talin            ###   ########.fr       */
+/*   Updated: 2024/10/04 09:26:28 by talin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,18 @@ int	ft_printf_hex(t_format new, va_list ptr)
 
 	count = 0;
 	n = va_arg(ptr, unsigned int);
-	len = ft_nbrlen(n);
-	if (!n && !new.precision && new.dot)
-		len = 0;
+	len = ft_nbrlen(n) * (!(!n && !new.precision && new.dot));
 	if (new.precision < 0 || new.precision < len || !new.dot)
 		new.precision = len;
 	new.width -= 2 * (new.sharp && n);
-	if (!new.minus && new.width > new.precision)
-		count += ft_putnchar(' ', new.width - new.precision);
-	count += ft_putnstr(ft_sharp(new), 2 * (new.sharp && new.zero && n));
 	if (!new.minus && new.width > new.precision \
 	&& (!new.dot || new.neg_prec) && new.zero)
-		count += ft_putnchar('0', new.width - new.precision);
+		count += ft_putnstr(ft_sharp(new), 2 * (new.sharp && new.zero && n)) + \
+		ft_putnchar('0', new.width - new.precision);
+	else if (!new.minus && new.width > new.precision)
+		count += ft_putnchar(' ', new.width - new.precision) + \
+		ft_putnstr(ft_sharp(new), 2 * (new.sharp && new.zero && n));
+	count += ft_putnstr(ft_sharp(new), 2 * (new.sharp && !new.zero && n));
 	count += ft_putnchar('0', new.precision - len);
 	if (len)
 		count += ft_recursive_hex(new, n, n);
