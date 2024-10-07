9327b07bb6e01012dda6097f51c39ef765339f33
Tauk Tauk Lin <talin@z3t5c1.42bangkok.com>
2024-10-07 16:56:42 +0700

    gnl

--- so_long/src/map_reader.c
+++ so_long/src/map_reader.c
@@ -6,32 +6,48 @@
 /*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 15:09:46 by talin             #+#    #+#             */
-/*   Updated: 2024/10/07 16:46:41 by talin            ###   ########.fr       */
+/*   Updated: 2024/10/07 16:56:27 by talin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-// static int	ft_read_size(char *filename)
-// {
-// 	int		n;
-// 	int		fd;
-// 	char	c;
+static int	ft_read_size(char *filename)
+{
+	int		n;
+	int		fd;
+	char	c;
 
-// 	n = 0;
-// 	fd = open(filename, O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		write(2, "map error\n", 10);
-// 		close(fd);
-// 		return (1);
-// 	}
-// 	while (read(fd, &c, 1) > 0)
-// 		n++;
-// 	return (n);
-// }
+	n = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		write(2, "map error\n", 10);
+		close(fd);
+		return (1);
+	}
+	while (read(fd, &c, 1) > 0)
+		n++;
+	return (n);
+}
 
 int	main(void)
 {
-	ft_printf("hello %s\n", "world");
+	int		fd;
+	char	*str;
+
+	fd = open("map.ber", O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("map error\n");
+		close(fd);
+		return (1);
+	}
+	str = get_next_line(fd);
+	ft_printf("%s\n", str);
+	while (str == NULL)
+	{
+		str = get_next_line(fd);
+		ft_printf("%s\n", str);
+	}
 }
