de2a21e9447147dca6929b1600e8f93f508fed03
Tauk Tauk Lin <talin@z3t5c1.42bangkok.com>
2024-10-07 16:51:14 +0700

    main libft

--- so_long/src/map_reader.c
+++ so_long/src/map_reader.c
@@ -1,19 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   map_reader.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 10:45:00 by talin             #+#    #+#             */
-/*   Updated: 2024/10/07 16:30:09 by talin            ###   ########.fr       */
+/*   Created: 2024/10/07 15:09:46 by talin             #+#    #+#             */
+/*   Updated: 2024/10/07 16:46:41 by talin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdbool.h>
-# include "libft.h"
-#endif
+#include "../include/so_long.h"
+
+// static int	ft_read_size(char *filename)
+// {
+// 	int		n;
+// 	int		fd;
+// 	char	c;
+
+// 	n = 0;
+// 	fd = open(filename, O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		write(2, "map error\n", 10);
+// 		close(fd);
+// 		return (1);
+// 	}
+// 	while (read(fd, &c, 1) > 0)
+// 		n++;
+// 	return (n);
+// }
+
+int	main(void)
+{
+	ft_printf("hello %s\n", "world");
+}
