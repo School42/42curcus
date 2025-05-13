b651c0c60b09466ddafd1df3ad13d7463ea77bfa
Tauk Tauk Lin <talin@z1t5c3.42bangkok.com>
2025-05-13 13:43:40 +0700

    blueprint"

---
+++ blueprint_mlx/include/cube.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: talin <talin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 17:34:38 by talin             #+#    #+#             */
+/*   Updated: 2025/05/13 10:24:07 by talin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+# define CUBE_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <stdio.h>
+
+#endif
