16161386aa691e5c84dabffb022e98fbad887d25
Tauk Tauk Lin <talin@z2t9c3.42bangkok.com>
2025-09-16 16:02:35 +0700

    ex02

--- cpp/module9/ex02/PmergeMe.hpp
+++ cpp/module9/ex02/PmergeMe.hpp
@@ -12,14 +12,23 @@
 #include <limits>
 #include <sstream>
 #include <iterator>
+#include <cmath>
 
 class PmergeMe
 {
+	private:
+		template <typename T> void merge_insert(T &container, int level);
+		template <typename T> void swap_pairs(T it, int level);
 	public:
 		PmergeMe();
 		~PmergeMe();
 		PmergeMe(const PmergeMe &other);
 		PmergeMe &operator=(const PmergeMe &other);
+		void sort_list(std::list<int> &list);
+		void sort_deque(std::deque<int> &deque);
+		static int comps;
 };
+
+long jacobsthal_value(long n);
 
 #endif
